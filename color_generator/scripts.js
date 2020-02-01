b28b0f63102a79ce9b3c880a81bccb711c34c00f
Dominic Jones-Jackson <44783114+sethveeper@users.noreply.github.com>
2020-02-01 14:48:30 -0600

    New project aaaaah
    Also, we edited the index to be simpler and less useful to people that aren't me.

---
+++ color_generator/scripts.js
@@ -0,0 +1,32 @@
+document.getElementById("button").addEventListener("click", changeColor);
+
+function changeColor() {
+  var newRed = Math.floor((Math.random() * 255));
+  var newGreen = Math.floor((Math.random() * 255));
+  var newBlue = Math.floor((Math.random() * 255));
+
+  var myColor = new Color(newRed, newGreen, newBlue);
+
+  document.getElementById("body").style.backgroundColor = myColor.hex;
+  document.getElementById("color").innerHTML = "<h3>Current Color:</h3><p>" + myColor.hex + "</p>"
+}
+
+function Color(red, green, blue) {
+
+  this.Parse = function(value) {
+    var output = "";
+    if (value <= 16)
+      output = "0" + value.toString(16);
+    else if (value >= 255)
+      output = "ff";
+    else
+      output = value.toString(16);
+      
+    return output;
+  };
+
+  this.red = this.Parse(red);
+  this.green = this.Parse(green);
+  this.blue = this.Parse(blue);
+  this.hex = "#" + this.red + this.green + this.blue
+}
