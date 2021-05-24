4cb1463f8df84e087a5284029a72c69026656368
Dominic Jones-Jackson <sethveeper@gmail.com>
2021-05-24 07:49:49 -0500

    Add some stuff for Eli

---
+++ eliSite/wishlist.js
@@ -0,0 +1,5 @@
+var wishlist = [
+    {"title": "Seth's JSON Card", "link": "http://freefall.purrsia.com/", "descript": "Just a test for reading JSON."},
+    {"title": "Seth's JSON Card", "link": "http://freefall.purrsia.com/", "descript": "Just a test for reading JSON."},
+    {"title": "Seth's JSON Card", "link": "http://freefall.purrsia.com/", "descript": "Just a test for reading JSON."}
+];
