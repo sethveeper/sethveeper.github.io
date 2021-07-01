5d68461221b3ecdf693e80ae29fc53675a7c773b
Dominic Jones-Jackson <sethveeper@gmail.com>
2021-07-01 16:05:17 -0500

    Update wishlist.js

--- eliSite/wishlist/wishlist.js
+++ eliSite/wishlist/wishlist.js
@@ -1,6 +1,7 @@
 var wishlist = [
 {"Title":"Cat Headphones","Link":"https://www.amazon.com/Brookstone-Wired-Cat-Ear-Headphones/dp/B015OR9R8O","Descript":"I like these ones because the ears are also speakers. I suppose I\u0027m not married to that feature, though. Annoyingly, though, they\u0027re unavailable at this link, so I have no idea how much they\u0027re supposed to cost."},
-{"Title":"Cum Lube","Link":"https://bad-dragon.com/products/cumlube","Descript":"I\u0027m almost out...\r\n\r\n-\\_UwU_/-"},
-{"Title":"Toe Bean Stockings","Link":"https://toebeanies.com/","Descript":"This is where they originally come from, it seems. According to the site owner they are patent-pending, and other sources are therefor not necessarily legal?"},
-{"Title":"FF14: Band of Eternal Passion","Link":"https://store.finalfantasyxiv.com/ffxivstore/en-us/product/63","Descript":"This is another glamour item, that was once available in-game but as far as I know won\u0027t be available that way again."}
+{"Title":"FF14: Band of Eternal Passion","Link":"https://store.finalfantasyxiv.com/ffxivstore/en-us/product/63","Descript":"This is another glamour item, that was once available in-game but as far as I know won\u0027t be available that way again."},
+{"Title":"D\u0026D 5e Player\u0027s Handbook","Link":"https://dnd.wizards.com/products/tabletop-games/rpg-products/rpg_playershandbook","Descript":"Self-explanatory, I think. Not entirely sure how badly I actually need this, granted..."},
+{"Title":"D\u0026D 5e Ravnica Supplement","Link":"https://dnd.wizards.com/products/tabletop-games/rpg-products/guildmasters-guide-ravnica","Descript":"Information from this supplement is a little harder to find sometimes \u003Ew\u003E"},
+{"Title":"MtG: Adventures in the Forgotten Realms","Link":"https://magic.wizards.com/en/articles/archive/feature/adventures-forgotten-realms-product-overview-2021-06-29","Descript":"This is the D\u0026D themed set coming out next month. There\u0027s a few specific cards I\u0027m excited about (DergKnight!) but having taken a careful look I kind of just like the set design all aorund so far."}
 ];
